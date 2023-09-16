79aeb6ba91eaff28bb97ed8150084c62c36172c8
martin-18246 <145228112+martin-18246@users.noreply.github.com>
2023-09-16 22:01:56 +0200

    change "rotation" to "orbit"

--- NewCamera.cs
+++ NewCamera.cs
@@ -7,6 +7,6 @@ public class NewCamera : MonoBehaviour
     void Update()
     {
         transform.LookAt(new Vector3(0, 0, 0));
-        transform.Translate(Vector3.right * Time.deltaTime * Info.cameraRotationSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * Info.cameraOrbitSpeed);
     }
 }
