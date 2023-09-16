bc2bda8a2c912ba71ce9d55925e4cea393ecc97b
martin-18246 <145228112+martin-18246@users.noreply.github.com>
2023-09-16 21:45:52 +0200

    Create NewCamera.cs

---
+++ NewCamera.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class NewCamera : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        transform.LookAt(new Vector3(0, 0, 0));
+        transform.Translate(Vector3.right * Time.deltaTime * Info.cameraRotationSpeed);
+    }
+}
